e9bd79e28cfc5859cedd759067bdbe7248f4dced
bellkimkeith <157334752+bellkimkeith@users.noreply.github.com>
2024-07-28 10:46:02 +0800

    initial tab navigation setup

---
+++ screens/BookmarksScreen.js
@@ -0,0 +1,18 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const BookmarksScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>BookmarksScreen</Text>
+    </View>
+  );
+};
+
+export default BookmarksScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
